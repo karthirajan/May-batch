c715acb7a448ffa6e85d94f88a64e7c1cec086a9
Nisha2215 <karthirajan0706@gmail.com>
2019-06-08 16:31:04 +0530

    new one

---
+++ src/org/sample/Arr.java
@@ -0,0 +1,23 @@
+package org.sample;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+public class Arr {
+	public static void main(String[] args) {
+		List<Integer>li=new ArrayList();
+		li.add(10);
+		li.add(20);
+		li.add(10);
+		li.add(30);
+		li.add(40);
+		li.add(10);
+
+		
+			
+		for (int i = 0; i < li.size(); i++) {
+			System.out.println(li.get(i));
+			
+		}
+	}
+}
