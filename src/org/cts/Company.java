257adb13765ed8a75eda6cbe2c791096e9b45626
Nisha2215 <karthirajan0706@gmail.com>
2019-06-08 16:40:45 +0530

    codes are modified by tester1

---
+++ src/org/cts/Company.java
@@ -0,0 +1,19 @@
+package org.cts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Company {
+
+	public static void main(String[] args) {
+		
+		List li=new ArrayList();
+		li.add(10);
+		li.add(10f);
+		li.add(10l);
+		li.add("Farith");
+		li.add(20);
+
+		System.out.println(li);
+	}
+}
